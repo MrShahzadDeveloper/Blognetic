fde4b0c277783c82ed9a0ec7b3e8eedd53494523
Mr-Naqash-Developer <mr.naqash.developer@gmail.com>
2025-06-11 12:07:40 +0500

    Collabe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="bg-red-400 flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold text-white">This is Home Page</h1>
+    <div className="bg-Blue-400 flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold text-white">Is it Working!</h1>
     </div>
   )
 }
