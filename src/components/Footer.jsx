941272f9f7241f7a2bb2b14bb66a68c8caa50e09
Mr-Naqash-Developer <mr.naqash.developer@gmail.com>
2025-06-11 18:21:32 +0500

    Components are Done almost

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import logo from "../assets/logo.png";
 
 const Footer = () => {
+  const footerLinks = [
+    { name: "Home", id: 1 },
+    { name: "Blog", id: 2 },
+    { name: "About", id: 3 },
+    { name: "Contact Us", id: 4 },
+  ];
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="h-[1px] bg-primary " />
+      <div className="bg-[#FFFFFF] flex flex-col justify-center items-center my-10 gap-10 ">
+        <div>
+          <div className="flex gap-3 items-center cursor-pointer">
+            <img className="h-8 w-8 " src={logo} alt="blognetic" />
+            <h1 className="font-[800] text-[24px] text-[#333333] ">
+              Blognetic
+            </h1>
+          </div>
+        </div>
+        <div className="hidden sm:flex gap-5">
+          {footerLinks.map((links) => (
+            <h4 key={links.id} className="nav-text">
+              {links.name}
+            </h4>
+          ))}
+        </div>
+        <div className="bg-primary w-full flex justify-center items-center py-2">
+          <p className="text-white text-center ">
+            Copyright © 2025 Blognetic. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import logo from "../assets/logo.png";
+import { Search } from "lucide-react";
+import Button from "./Button";
+import { useState } from "react";
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const menuLinks = [
+    { name: "Home", id: 1 },
+    { name: "Blogs", id: 2 },
+    { name: "About", id: 3 },
+    { name: "Contact Us", id: 4 },
+    { name: "Terms & Conditions", id: 5 },
+  ];
+  const [visible, setVisible] = useState(false);
+  const toggleMenu = () => {
+    setVisible(!visible);
+  };
 
-export default Navbar
+  return (
+    <>
+      <div className="bg-[#FFFFFF] flex justify-between items-center py-4 px-8 sm:px-16">
+        <div className="flex gap-3 items-center cursor-pointer">
+          <img className="h-6 w-6 sm:h-8 sm:w-8 " src={logo} alt="blognetic" />
+          <h1 className="font-[800] text-[20px] sm:text-[24px] text-[#333333] ">
+            Blognetic
+          </h1>
+        </div>
+        <div className="hidden md:flex items-center gap-8">
+          {menuLinks.map((links) => (
+            <h4 key={links.id} className="hover:text-[#7C4EE4] cursor-pointer ">{links.name}</h4>
+          ))}
+          <Search strokeWidth={"0.75px"} />
+          <Button text="Contact Us" textColor="white" bgColor={"#7C4EE4"} />
+        </div>
+        <div onClick={toggleMenu} className="flex md:hidden cursor-pointer">
+          {visible ? <X size={24} /> : <Menu size={24} />}
+        </div>
+      </div>
+      {visible ? (
+        <div className="flex flex-col justify-center items-end mr-6">
+          <div className="bg-[#7f61c4] py-8 px-6 text-center flex flex-col gap-6 rounded-lg ">
+            {menuLinks.map((links) => (
+              <h4 className="text-white text-xl" key={links.id}>
+                {links.name}
+              </h4>
+            ))}
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default Navbar;
